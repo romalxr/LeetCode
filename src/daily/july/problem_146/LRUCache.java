fc554961d2c882c545b75f023b89cfa03aebc9d9
romalxr <romalxr@gmail.com>
2023-07-18 07:59:22 +0300

    solved more..

---
+++ src/daily/july/problem_146/LRUCache.java
@@ -0,0 +1,38 @@
+package daily.july.problem_146;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+
+class LRUCache {
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    private void makeRecently(int key){
+        int val = cache.get(key);
+        cache.remove(key);
+        cache.put(key,val);
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        makeRecently(key);
+        return cache.get(key);
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            cache.remove(key);
+        }
+        if (cache.size() >= capacity) {
+            Integer oldestKey = cache.keySet().iterator().next();
+            cache.remove(oldestKey);
+        }
+        cache.put(key,value);
+    }
+}
---
+++ src/daily/july/problem_146/Main.java
@@ -0,0 +1,16 @@
+package daily.july.problem_146;
+
+public class Main {
+    public static void main(String[] args) {
+        LRUCache obj = new LRUCache(2);
+        obj.put(1, 1);
+        obj.put(2, 2);
+        System.out.println(obj.get(1));
+        obj.put(3, 3);
+        System.out.println(obj.get(2));
+        obj.put(4, 4);
+        System.out.println(obj.get(1));
+        System.out.println(obj.get(3));
+        System.out.println(obj.get(4));
+    }
+}
